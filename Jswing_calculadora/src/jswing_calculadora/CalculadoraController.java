b3609970380511e89e053a30f1b4608f7cdd5503
smunpov1010 <smunpov1010@PC111918.dptoinformatica.iesfuengirola1.net>
2025-04-30 17:54:53 +0200

    commit

--- Jswing_calculadora/src/jswing_calculadora/CalculadoraController.java
+++ Jswing_calculadora/src/jswing_calculadora/CalculadoraController.java
@@ -1,54 +1,51 @@
 package jswing_calculadora;
+import javax.swing.*;
+import java.awt.event.*;
 
-//package prueba;
-//
-//import javax.swing.*;
-//import java.awt.event.*;
-//
-//public class CalculadoraController {
-//    private JTextField display;
-//    private double numero1, numero2, resultado;
-//    private String operacion;
-//
-//    public CalculadoraController(JTextField display, JButton[] botones) {
-//        this.display = display;
-//        for (JButton boton : botones) {
-//            boton.addActionListener(new ActionListener() {
-//                public void actionPerformed(ActionEvent e) {
-//                    manejarEvento(e.getActionCommand());
-//                }
-//            });
-//        }
-//    }
-//
-//    private void manejarEvento(String comando) {
-//        if (comando.matches("[0-9.]")) {
-//            display.setText(display.getText() + comando);
-//        } else if (comando.matches("[+\\-*/]")) {
-//            try {
-//                numero1 = Double.parseDouble(display.getText());
-//                operacion = comando;
-//                display.setText("");
-//            } catch (NumberFormatException ex) {
-//                display.setText("Error");
-//            }
-//        } else if (comando.equals("=")) {
-//            try {
-//                numero2 = Double.parseDouble(display.getText());
-//                switch (operacion) {
-//                    case "+": resultado = numero1 + numero2; break;
-//                    case "-": resultado = numero1 - numero2; break;
-//                    case "*": resultado = numero1 * numero2; break;
-//                    case "/": resultado = numero2 != 0 ? numero1 / numero2 : 0; break;
-//                }
-//                display.setText(String.valueOf(resultado));
-//            } catch (NumberFormatException ex) {
-//                display.setText("Error");
-//            }
-//        } else if (comando.equals("C")) {
-//            display.setText("");
-//            numero1 = numero2 = resultado = 0;
-//            operacion = "";
-//        }
-//    }
-//}
+public class CalculadoraController {
+    private JTextField display;
+    private double numero1, numero2, resultado;
+    private String operacion;
+
+    public CalculadoraController(JTextField display, JButton[] botones) {
+        this.display = display;
+        for (JButton boton : botones) {
+            boton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    manejarEvento(e.getActionCommand());
+                }
+            });
+        }
+    }
+
+    private void manejarEvento(String comando) {
+        if (comando.matches("[0-9.]")) {
+            display.setText(display.getText() + comando);
+        } else if (comando.matches("[+\\-*/]")) {
+            try {
+                numero1 = Double.parseDouble(display.getText());
+                operacion = comando;
+                display.setText("");
+            } catch (NumberFormatException ex) {
+                display.setText("Error");
+            }
+        } else if (comando.equals("=")) {
+            try {
+                numero2 = Double.parseDouble(display.getText());
+                switch (operacion) {
+                    case "+": resultado = numero1 + numero2; break;
+                    case "-": resultado = numero1 - numero2; break;
+                    case "*": resultado = numero1 * numero2; break;
+                    case "/": resultado = numero2 != 0 ? numero1 / numero2 : 0; break;
+                }
+                display.setText(String.valueOf(resultado));
+            } catch (NumberFormatException ex) {
+                display.setText("Error");
+            }
+        } else if (comando.equals("C")) {
+            display.setText("");
+            numero1 = numero2 = resultado = 0;
+            operacion = "";
+        }
+    }
+}
